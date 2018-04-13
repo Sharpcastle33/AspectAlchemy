c830130de208c7d300cfcb4a42efd05954e74ace
KingVictoria <TheRealKingVictoria@gmail.com>
2018-04-13 01:58:33 -0400

    Corrected some minor syntactical errors in the AlembicManager implementation that were revealed upon compilation

--- src/main/java/com/gmail/sharpcastle33/AlembicManager.java
+++ src/main/java/com/gmail/sharpcastle33/AlembicManager.java
@@ -9,17 +9,17 @@ public class AlembicManager {
   
   public ArrayList<Location> activeAlembics;
   
-  public AlembicManager(){
-    activeAlembics = loadActiveAlembics();
+  public AlembicManager(Plugin plugin){
+    activeAlembics = loadActiveAlembics(plugin);
   }
  
   public ArrayList<Location> loadActiveAlembics(Plugin plugin){
     ArrayList<Location> ret = new ArrayList<Location>();
 
-    ArrayList<HashMap<String, String>> serializedAlembics = plugin.getConfig().getList("active_alembics");
+    ArrayList<HashMap<String, Object>> serializedAlembics = (ArrayList<HashMap<String, Object>>) plugin.getConfig().get("active_alembics");
     if(serializedAlembics == null) return ret;
 
-    for(HashMap<String, String> map: serializedAlembics) {
+    for(HashMap<String, Object> map: serializedAlembics) {
 	double x = (double) map.get("x");
 	double y = (double) map.get("y");
 	double z = (double) map.get("z");
@@ -31,7 +31,7 @@ public class AlembicManager {
   }
   
   public void saveActiveAlembics(Plugin plugin){
-      ArrayList<HashMap<String, String>> serializedAlembics = new ArrayList<>();
+      ArrayList<HashMap<String, Object>> serializedAlembics = new ArrayList<>();
       for(Location loc: activeAlembics) {
 	  HashMap<String, Object> map = new HashMap<>();
 	  map.put("x", loc.getX());
