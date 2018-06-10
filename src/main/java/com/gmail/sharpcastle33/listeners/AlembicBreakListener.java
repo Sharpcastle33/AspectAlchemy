6a899dd256d95da0f8b6cb93ef1763f762fca9c1
Stephen / Sharp <Sharpcastle33@users.noreply.github.com>
2018-06-09 23:42:31 -0400

    wip break listener

--- src/main/java/com/gmail/sharpcastle33/listeners/AlembicBreakListener.java
+++ src/main/java/com/gmail/sharpcastle33/listeners/AlembicBreakListener.java
@@ -2,6 +2,9 @@ package com.gmail.sharpcastle33.listeners;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BrewingStand;
+import org.bukkit.block.Chest;
+import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -13,15 +16,17 @@ public class AlembicBreakListener implements Listener{
 		Block b = event.getBlock();
 		
 		if(b.getType() == Material.FURNACE) {
-			
+			Furnace furnaceState = (Furnace) b.getState();
 		}
 		
 		if(b.getType() == Material.BREWING_STAND) {
-			
+			BrewingStand furnaceState = (BrewingStand) b.getState();
+
 		}
 		
 		if(b.getType() == Material.CHEST) {
-			
+			Chest furnaceState = (Chest) b.getState();
+
 		}
 	}
 }
