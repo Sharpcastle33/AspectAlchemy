eb49bddb649d314851870108f86fad1e2f202af1
Stephen / Sharp <Sharpcastle33@users.noreply.github.com>
2018-06-10 00:19:09 -0400

    more breaks

--- src/main/java/com/gmail/sharpcastle33/listeners/AlembicBreakListener.java
+++ src/main/java/com/gmail/sharpcastle33/listeners/AlembicBreakListener.java
@@ -1,5 +1,6 @@
 package com.gmail.sharpcastle33.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -10,6 +11,9 @@ import org.bukkit.block.Furnace;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import com.gmail.sharpcastle33.handlers.AlembicHandler;
 
 public class AlembicBreakListener implements Listener{
 	
@@ -22,11 +26,16 @@ public class AlembicBreakListener implements Listener{
 		if(b.getType() == Material.FURNACE) {
 			Furnace furnaceState = (Furnace) b.getState();
 			if(furnaceState.getInventory().getName().equals(AlembicCreationListener.ALEMBIC_FURNACE_NAME)) {
-				if(b.getRelative(BlockFace.UP) instanceof Chest) {
+				
+				if(event.isCancelled()) { return; }
+				
+				if(b.getRelative(BlockFace.UP).getState() instanceof Chest) {
 					Chest chestState = (Chest) b.getRelative(BlockFace.UP).getState();
 					if (chestState.getInventory().getItem(17).hasItemMeta() && chestState.getInventory().getItem(17).getItemMeta().getDisplayName().equals(ChatColor.RED + "In Progress")) {
 						event.setCancelled(true);
 						event.getPlayer().sendMessage(IN_PROGRESS_MESSAGE);
+					}else {
+						
 					}
 				}
 			}
@@ -35,11 +44,16 @@ public class AlembicBreakListener implements Listener{
 		else if(b.getType() == Material.BREWING_STAND) {
 			BrewingStand brewingStandState = (BrewingStand) b.getState();
 			if(brewingStandState.getInventory().getName().equals(AlembicCreationListener.ALEMBIC_BREWINGSTAND_NAME)) {
-				if(b.getRelative(BlockFace.DOWN) instanceof Chest) {
+				
+				if(event.isCancelled()) { return; }
+				
+				if(b.getRelative(BlockFace.DOWN).getState() instanceof Chest) {
 					Chest chestState = (Chest) b.getRelative(BlockFace.DOWN).getState();
 					if (chestState.getInventory().getItem(17).hasItemMeta() && chestState.getInventory().getItem(17).getItemMeta().getDisplayName().equals(ChatColor.RED + "In Progress")) {
 						event.setCancelled(true);
 						event.getPlayer().sendMessage(IN_PROGRESS_MESSAGE);
+					}else {
+						
 					}
 				}
 			}
@@ -55,8 +69,32 @@ public class AlembicBreakListener implements Listener{
 				if (chestState.getInventory().getItem(17).hasItemMeta() && chestState.getInventory().getItem(17).getItemMeta().getDisplayName().equals(ChatColor.RED + "In Progress")) {
 					event.setCancelled(true);
 					event.getPlayer().sendMessage(IN_PROGRESS_MESSAGE);
+				}else {
+					breakAlembicChest(b);
+					b.getRelative(BlockFace.UP).breakNaturally();
+					b.getRelative(BlockFace.DOWN).breakNaturally();
 				}
 			}
+		}
+	}
+	
+	public void breakAlembicChest(Block b) {
+		if(b.getState() instanceof Chest) {
+			Chest chestState = (Chest) b.getState();
+		
+			for(ItemStack i : AlembicHandler.getIngredients(chestState)) {
+				b.getLocation().getWorld().dropItemNaturally(b.getLocation(), i);
+			}
+			
+			for(ItemStack i : AlembicHandler.getShamanSaps(chestState)) {
+				b.getLocation().getWorld().dropItemNaturally(b.getLocation(), i);
+			}
+			
+			chestState.getInventory().clear();
+			
+			b.breakNaturally();
+		}else {
+			Bukkit.getServer().getLogger().warning("breakAlembicChest was called, but the block is not a chest!" + b.getLocation().toString());
 		}
 	}
 }
