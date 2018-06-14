fc33a5b23c07ff928a17f1f47351a0f5383025db
KingVictoria <TheRealKingVictoria@gmail.com>
2018-06-13 21:03:36 -0400

    Fixed brewing tool

--- src/main/java/com/gmail/sharpcastle33/listeners/AdminToolsListener.java
+++ src/main/java/com/gmail/sharpcastle33/listeners/AdminToolsListener.java
@@ -25,57 +25,62 @@ import com.gmail.sharpcastle33.potions.PotionManager;
 
 import net.md_5.bungee.api.ChatColor;
 
-public class AdminToolsListener implements Listener{
+public class AdminToolsListener implements Listener {
 
 	@EventHandler
 	public void resonator(PlayerInteractEvent event) {
-		
+
 		final String ASPECT_ADMIN_TOOL = ChatColor.RED + "Secret Admin Aspect Tool";
 		final String POTION_ADMIN_TOOL = ChatColor.RED + "Secret Admin Potion Tool";
 		final String INSTANT_ADMIN_TOOL = ChatColor.RED + "Secret Admin Brewing Tool";
 
-		/* DEBUG CODE 
-		if(event.getHand() == EquipmentSlot.HAND) event.getPlayer().sendMessage(ChatColor.DARK_PURPLE+"YOUR HAND IS A HAND!");
-		if(event.getAction() == Action.RIGHT_CLICK_AIR) event.getPlayer().sendMessage(ChatColor.DARK_PURPLE+"YOU RIGHT CLICKED THE AIR!");
-		if(event.getAction() == Action.RIGHT_CLICK_BLOCK) event.getPlayer().sendMessage(ChatColor.DARK_PURPLE+"YOU RIGHT CLICKED A BLOCK!");
-		/* END DEBUG CODE */
-		
+		/*
+		 * DEBUG CODE if(event.getHand() == EquipmentSlot.HAND)
+		 * event.getPlayer().sendMessage(ChatColor.DARK_PURPLE+"YOUR HAND IS A HAND!");
+		 * if(event.getAction() == Action.RIGHT_CLICK_AIR)
+		 * event.getPlayer().sendMessage(ChatColor.
+		 * DARK_PURPLE+"YOU RIGHT CLICKED THE AIR!"); if(event.getAction() ==
+		 * Action.RIGHT_CLICK_BLOCK) event.getPlayer().sendMessage(ChatColor.
+		 * DARK_PURPLE+"YOU RIGHT CLICKED A BLOCK!"); /* END DEBUG CODE
+		 */
+
 		if (event.getHand() == EquipmentSlot.HAND
 				&& (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)) {
 			Player p = event.getPlayer();
 			ItemStack main = p.getInventory().getItemInMainHand();
 			ItemStack off = p.getInventory().getItemInOffHand();
-			
-			if(main.hasItemMeta() && off.hasItemMeta()) {
+
+			if (main.hasItemMeta() && off.hasItemMeta()) {
 				ItemMeta mainMeta = main.getItemMeta();
 				ItemMeta offMeta = off.getItemMeta();
-				
-				if(mainMeta.hasDisplayName() && mainMeta.getDisplayName().equals(ASPECT_ADMIN_TOOL)) {
-					if(offMeta.hasDisplayName()) {
+
+				if (mainMeta.hasDisplayName() && mainMeta.getDisplayName().equals(ASPECT_ADMIN_TOOL)) {
+					if (offMeta.hasDisplayName()) {
 						Map<Aspect, Integer> temp = AspectManager.getAspects(off);
 						p.sendMessage(ChatColor.BLUE + "This item has the following aspects:");
-						for(Aspect a : temp.keySet()) {
+						for (Aspect a : temp.keySet()) {
 							p.sendMessage(ChatColor.GOLD + a.name() + ": " + temp.get(a));
 						}
 					}
 				}
-				
-				if(mainMeta.hasDisplayName() && mainMeta.getDisplayName().equals(POTION_ADMIN_TOOL)) {
+
+				if (mainMeta.hasDisplayName() && mainMeta.getDisplayName().equals(POTION_ADMIN_TOOL)) {
 					p.sendMessage(ChatColor.BLUE + "There are " + PotionManager.potions.size() + " potions loaded.");
-					for(CustomPotion pot : PotionManager.potions.keySet()) {
+					for (CustomPotion pot : PotionManager.potions.keySet()) {
 						p.sendMessage(ChatColor.GOLD + pot.name());
 					}
 				}
-				
-				if(mainMeta.hasDisplayName() && mainMeta.getDisplayName().equals(INSTANT_ADMIN_TOOL)) {
-					if(AlembicHandler.isAlembic(event.getClickedBlock())) {
-						
+
+				if (mainMeta.hasDisplayName() && mainMeta.getDisplayName().equals(INSTANT_ADMIN_TOOL)) {
+					if (AlembicHandler.isAlembic(event.getClickedBlock())) {
+
 						Chest chest = (Chest) event.getClickedBlock().getState();
-						
+
 						ItemStack progress = chest.getInventory().getItem(17);
 						ItemMeta progressMeta = progress.hasItemMeta() ? progress.getItemMeta() : null;
-						if (progressMeta == null) {
-							AlembicHandler.completeAlchemy((Chest) event.getClickedBlock().getState(), (BrewingStand) event.getClickedBlock().getRelative(BlockFace.UP).getState());
+						if (progressMeta != null && progressMeta.getDisplayName().equals(ChatColor.GREEN + "Start Alchemy")) {
+							AlembicHandler.completeAlchemy((Chest) event.getClickedBlock().getState(),
+									(BrewingStand) event.getClickedBlock().getRelative(BlockFace.UP).getState());
 						} else {
 							List<String> lore = new ArrayList<>();
 							lore.add(ChatColor.RED + "Time Remaining: " + 1 + "min");
@@ -86,6 +91,6 @@ public class AdminToolsListener implements Listener{
 					}
 				}
 			}
-		}	
-	 }
+		}
+	}
 }
