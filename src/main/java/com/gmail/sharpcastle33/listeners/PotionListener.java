fa2a97a23d023a98fb87aca779b30d24357e9d2c
Sharpcastle33 <sharpcastle33@gmail.com>
2019-02-07 01:13:59 -0500

    Started custom effect support, added Antidote

---
+++ src/main/java/com/gmail/sharpcastle33/listeners/PotionListener.java
@@ -0,0 +1,35 @@
+package com.gmail.sharpcastle33.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerItemConsumeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffectType;
+import net.md_5.bungee.api.ChatColor;
+
+public class PotionListener implements Listener {
+  
+  String ANTIDOTE = ChatColor.YELLOW + "Antidote";
+  String STRONG_ANTIDOTE = ChatColor.YELLOW + "Potent Antidote";
+  
+  @EventHandler
+  public void onDrink(PlayerItemConsumeEvent event) {
+    ItemStack i = event.getItem();
+    Player p = event.getPlayer();
+    
+    if(i.hasItemMeta() && i.getItemMeta().hasDisplayName()) {
+      ItemMeta meta = i.getItemMeta();
+      
+      //ANTIDOTE
+      if(meta.getDisplayName().equals(ANTIDOTE) || meta.getDisplayName().equals(STRONG_ANTIDOTE)) {
+        if(p.hasPotionEffect(PotionEffectType.POISON)) {
+          p.removePotionEffect(PotionEffectType.POISON);
+        }
+      }
+      
+    }
+  }
+
+}
