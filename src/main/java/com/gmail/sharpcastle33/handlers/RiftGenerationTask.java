49a52ce975e7ab87b8cd827eabb7c01c8f6ee053
Sharpcastle33 <sharpcastle33@gmail.com>
2019-02-12 00:03:12 -0500

    added tick task

---
+++ src/main/java/com/gmail/sharpcastle33/handlers/RiftGenerationTask.java
@@ -0,0 +1,16 @@
+package com.gmail.sharpcastle33.handlers;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class RiftGenerationTask extends BukkitRunnable{
+  
+  public RiftGenerationTask() {
+    
+  }
+  
+  @Override
+  public void run() {
+    
+  }
+
+}
